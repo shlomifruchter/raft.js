aeebc10b268c82cca9332c416902e745a7a0a75c
Shlomi Fruchter <shlomi.fruchter@gmail.com>
2014-05-26 16:47:49 +0300

    remote old files

--- start.bat
+++
@@ -1,8 +0,0 @@
-REM start node server logger 0
-rmdir /s /q data
-rmdir /s /q logs
-start node server raft 1
-start node server raft 2
-start node server raft 3
-start node server raft 4
-start node server raft 5
